4ed5f2f1e82c692be523d1381a3cd9d360bbb1ea
Sohum Chatterjee <Sohum_Chatterjee@brown.edu>
2014-05-04 16:19:10 -0400

    testing

--- src/app/backend/user/AppUser.java
+++ src/app/backend/user/AppUser.java
@@ -38,9 +38,8 @@ public class AppUser implements User, Serializable {
 	 * @param name
 	 */
 	public AppUser(String name) {
-
+		
 		this.username = name; //initialize name
-
 		
 		this.allItems = new HashSet<Item>(); //initialize set of all items 
 		this.itemNames = new HashSet<String>(); //initialize set of all itemNames
@@ -51,7 +50,7 @@ public class AppUser implements User, Serializable {
 		
 		this.wardrobeList = new ArrayList<Wardrobe>(); //initialize empty list
 		wardrobeList.add(new AppWardrobe("Home Closet", WardrobeType.CLOSET)); //add default home closet
-
+		
 		this.allCategories = new HashSet<Category>(); //initialize category set
 		addCategory("shirts");
 		addCategory("dresses");
@@ -61,20 +60,12 @@ public class AppUser implements User, Serializable {
 		addCategory("sweaters");
 		addCategory("jeans");
 		addCategory("jackets");
-
-
-		this.allItems = new HashSet<Item>(); //initialize set of all items 
-		this.itemNames = new HashSet<String>(); //initialize set of all itemNames
-
-		this.outfitList = new ArrayList<Outfit>(); //initialize list of outfits
-
-		this.tagsMap = new HashMap<String, HashSet<Item>>(); //initialize map of tags to items
-
+		
 		this.autosuggest = new Autosuggest(tagsMap); //set up autosuggest
 		autosuggest.setUp();
-
+		
 		this.tagsuggester = new StubTagger();
-
+		
 	}
 
 	@Override
@@ -96,7 +87,7 @@ public class AppUser implements User, Serializable {
 	public int howManyOutfits() {
 		return this.outfitList.size();
 	}
-
+	
 	@Override
 	public Wardrobe searchWardrobe(String name){
 		Wardrobe result = null;
@@ -134,19 +125,23 @@ public class AppUser implements User, Serializable {
 		addItem(toAdd);
 
 	}
-
+	
+	/**
+	 * add an Item object
+	 * @param item
+	 */
 	private void addItem(Item item) {
 
 		allItems.add(item);
-
+		
 		//add items tags
 		HashSet<String> tagsToAdd = item.getTags();
-
+		
 		for (String tag: tagsToAdd){
 
 			if (tagsMap.containsKey(tag)){
 				tagsMap.get(tag).add(item);
-
+				
 			}
 			else{
 				HashSet<Item> itemSet = new HashSet<Item>();
@@ -166,39 +161,38 @@ public class AppUser implements User, Serializable {
 
 	@Override
 	public Collection<Item> search(String searchTerms) {
-
+		
 		for (Item item: allItems){
 			item.resetScore();
 		}
-
+		
 		HashSet<Item> matchingItems = new HashSet<Item>();
 		ArrayList<Item> toReturn = new ArrayList<Item>();
 
-
+		
 
 		Set<String> matchingTags = autosuggest.lookup(searchTerms);
-
-
+		
+		
 		for (String tag: matchingTags){
-
+			
 			HashSet<Item> items = tagsMap.get(tag);
-
+			
 			for (Item item: items){
 				item.incrementScore();
 				matchingItems.add(item);
 			}
-
+			
 		}
-
+		
 		for (Item item: matchingItems){
 			toReturn.add(item);
 		}
-
+		
 		Collections.sort(toReturn);
-
+		
 		return toReturn;
 	}
-
 
 
 
--- src/app/backend/user/TestingScript.java
+++ src/app/backend/user/TestingScript.java
@@ -4,48 +4,105 @@ import java.util.ArrayList;
 
 import app.backend.interfaces.Item;
 import app.backend.interfaces.User;
+import app.backend.user.AppWardrobe.WardrobeType;
 
 public class TestingScript {
 
 	public static void main(String[] args) {
-		Saver saved = new Saver("Sohum");
-		AppUser user = (AppUser) saved.getUser();
 		
-		user.addWardrobe("away");
-		user.addWardrobe("herenorthere");
-		  AppItem redShirt = new AppItem(user, user.searchWardrobe("away"), "redShirt", null);
-		  
-		  redShirt.addTag("red"); redShirt.addTag("shirt");
-		  redShirt.addTag("fits like a condom");
-		  
-		  AppItem blueShirt = new AppItem(user, user.searchWardrobe("away"), "blueShirt", null);
-		  
-		  blueShirt.addTag("blue"); blueShirt.addTag("shirt");
-		  blueShirt.addTag("fits like a sack");
-		  
-		  AppItem greenShirt = new AppItem(user, user.searchWardrobe("away"), "greenShirt", null);
-		  
-		  greenShirt.addTag("green"); greenShirt.addTag("shirt");
-		  greenShirt.addTag("fits like a glove");
-		  
-		  AppItem redShirt2 = new AppItem(user, user.searchWardrobe("away"), "redShirt2", null);
-		  
-		  redShirt2.addTag("red"); redShirt2.addTag("shirt");
-		  redShirt2.addTag("fits like a glove");
-		  
-		  user.addItem("redShirt", "away", "red", "red", null, new ArrayList<String>() )
-		  user.addI
-
-		String toSearch = " red shirt fits like a glove";
-
-		ArrayList<Item> result = (ArrayList<Item>) user.search(toSearch);
-
-		System.out.println(result.size());
-
-		for (int i = 0; i < result.size(); i++) {
-			System.out.println(result.get(i).getName());
-		}
-
-		saved.saveData();
+		
+		
+		AppUser u = new AppUser("TestOwner");
+		System.out.println(u.howManyWardrobes());
+		
+		
+		
+		AppItem item1 = new AppItem(u,u.searchWardrobe("Home Closet"), "item one", "");
+		AppItem item2 = new AppItem(u,u.searchWardrobe("Home Closet"), "item two", "");
+		AppItem item3 = new AppItem(u,u.searchWardrobe("Home Closet"), "item three", "");
+		AppItem item4 = new AppItem(u,u.searchWardrobe("Home Closet"), "item four", "");
+		AppItem item5 = new AppItem(u,u.searchWardrobe("Home Closet"), "item five", "");
+		AppItem item6 = new AppItem(u,u.searchWardrobe("Home Closet"), "item six", "");
+		AppItem item7 = new AppItem(u,u.searchWardrobe("Home Closet"), "item seven", "");
+		AppItem item8 = new AppItem(u,u.searchWardrobe("Home Closet"), "item eight", "");
+		AppItem item9 = new AppItem(u,u.searchWardrobe("Home Closet"), "item nine", "");
+		AppItem item10 = new AppItem(u,u.searchWardrobe("Home Closet"), "item ten", "");
+		AppItem item11 = new AppItem(u,u.searchWardrobe("Home Closet"), "item eleven", "");
+		AppItem item12 = new AppItem(u,u.searchWardrobe("Home Closet"), "item twelve", "");
+		AppItem item13 = new AppItem(u,u.searchWardrobe("Home Closet"), "item thirteen", "");
+		AppItem item14 = new AppItem(u,u.searchWardrobe("Home Closet"), "item fourteen", "");
+		AppItem item15 = new AppItem(u,u.searchWardrobe("Home Closet"), "item fifteen", "");
+		AppItem item16 = new AppItem(u,u.searchWardrobe("Home Closet"), "item sixteen", "");
+		AppItem item17 = new AppItem(u,u.searchWardrobe("Home Closet"), "item seventeen", "");
+		
+		item1.addTag(" green holidays awesome");
+		
+		
+		
+		for (String s : item1.getTags())
+			System.out.println(s);
+		//u.addItem(item1);
+		
+		System.out.println(u.search("shi"));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		Saver saved = new Saver("Sohum");
+//		AppUser user = (AppUser) saved.getUser();
+//		
+//		  user.addWardrobe("away", WardrobeType.CLOSET);
+//		  user.addWardrobe("herenorthere",WardrobeType.CLOSET);
+//		  AppItem redShirt = new AppItem(user, user.searchWardrobe("away"), "redShirt", null);
+//		  
+//		  redShirt.addTag("red"); redShirt.addTag("shirt");
+//		  redShirt.addTag("fits like a condom");
+//		  
+//		  AppItem blueShirt = new AppItem(user, user.searchWardrobe("away"), "blueShirt", null);
+//		  
+//		  blueShirt.addTag("blue"); blueShirt.addTag("shirt");
+//		  blueShirt.addTag("fits like a sack");
+//		  
+//		  AppItem greenShirt = new AppItem(user, user.searchWardrobe("away"), "greenShirt", null);
+//		  
+//		  greenShirt.addTag("green"); greenShirt.addTag("shirt");
+//		  greenShirt.addTag("fits like a glove");
+//		  
+//		  AppItem redShirt2 = new AppItem(user, user.searchWardrobe("away"), "redShirt2", null);
+//		  
+//		  redShirt2.addTag("red"); redShirt2.addTag("shirt");
+//		  redShirt2.addTag("fits like a glove");
+//		  
+//		  user.addItem("redShirt", "away", "red", "red", null, new ArrayList<String>() );
+//		  
+//
+//		String toSearch = " red shirt fits like a glove";
+//
+//		ArrayList<Item> result = (ArrayList<Item>) user.search(toSearch);
+//
+//		System.out.println(result.size());
+//
+//		for (int i = 0; i < result.size(); i++) {
+//			System.out.println(result.get(i).getName());
+//		}
+//
+//		saved.saveData();
 	}
 }
